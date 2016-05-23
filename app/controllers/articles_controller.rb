39b0ed14358f30e49af88d687ee301b4fa01ce07
Rails Test Flight <taken.reep@gmail.com>
2016-05-23 18:05:36 +0530

    Code Redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,14 @@
 class ArticlesController < ApplicationController
+	#before_action :set_article, only: [:show, :edit, :update, :destroy]
+	before_action :set_article, except: [:index, :create, :update, :new]
+
 	def index
 		@articles = Article.all
 	end
 
 	def show
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
+		
 	end
 
 	def new
@@ -12,7 +16,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])	
+		#@article = Article.find(params[:id])
 	end
 
 	def create
@@ -28,7 +32,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
 		if @article.update(article_params)
 			flash[:notice] = "Article was successfully updated"
 			redirect_to article_path(@article)
@@ -38,13 +42,17 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
 		@article.destroy
 		flash[:notice] = "Article was successfully deleted"
 		redirect_to articles_path
 	end
 
 	private
+		def set_article
+			@article = Article.find(params[:id])
+		end
+		
 		def article_params
 			params.require(:article).permit(:title, :description)
 		end
